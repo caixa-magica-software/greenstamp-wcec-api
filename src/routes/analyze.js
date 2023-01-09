8b470f5a4b58996235d1b0b36502b1abdf7ba55c
Miguel Tavares <hmigueltavares@gmail.com>
2023-01-09 22:42:01 +0000

    added body verification

--- src/routes/analyze.js
+++ src/routes/analyze.js
@@ -2,19 +2,28 @@ const axios = require('axios')
 const router = require('express').Router()
 
 router.post('/', (req, res) => {
-  console.log(req.body)
   const { appName, packageName, version, url, metadata, tests } = req.body
-  setTimeout(() => {
-    axios.post(process.env.DELIVER_RESULTS_ENDPOINT, {
-      appName: appName,
-      packageName: packageName,
-      version: version,
-      timestamp: Date.now(),
-      results: doTests(tests)
-    })
-  }, 1 * 1000)
-  res.status(200).send()
+  if(appName == null || appName == "") res.send(400).send({ message: "appName name cannot be null or empty" });
+  else if(packageName == null || packageName == "") res.send(400).send({ message: "packageName name cannot be null or empty" });
+  else if(version != null && version == "") res.send(400).send({ message: "version name cannot be null or empty" });
+  else if(url != null && url == "") res.send(400).send({ message: "url name cannot be null or empty" });
+  else if(metadata != null && metadata == "") res.send(400).send({ message: "metadata name cannot be null or empty" });
+  else if(tests != null && tests.length == 0) res.send(400).send({ message: "tests name cannot be null or empty" });
+  else {
+    setTimeout(() => execute(req.body), 10 * 1000)
+    res.status(200).send()
+  }
 })
+
+const execute = (body) => {
+  axios.post(process.env.DELIVER_RESULTS_ENDPOINT, {
+    appName: body.appName,
+    packageName: body.packageName,
+    version: body.version,
+    timestamp: Date.now(),
+    results: doTests(body.tests)
+  })
+}
 
 const doTests = (tests) => {
   return tests.map(test => ({
