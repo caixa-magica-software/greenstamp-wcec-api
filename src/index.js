c950010d0acd412bfdb9813fb39860076df4c984
Miguel Tavares <hmigueltavares@gmail.com>
2023-02-07 21:03:16 +0000

    fix: changed port 4000 -> 3000

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ app.use(express.json())
 app.get('/_/health', (req, res) => res.sendStatus(200));
 app.use('/api/analyze', require('./routes/analyze'))
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log(`Analyzer-Dummy is listening at ${process.env.PORT || 4000}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Analyzer-Dummy is listening at ${process.env.PORT || 3000}`);
 });
