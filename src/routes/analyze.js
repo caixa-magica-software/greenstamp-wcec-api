ddc45ba675d40cbc1bcd4686fb5e927ab34d8373
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-06-30 21:21:35 +0100

    Fix run multi docker containers

--- src/routes/analyze.js
+++ src/routes/analyze.js
@@ -141,7 +141,7 @@ const doTests = (resultsPath, apkPath, tests) => {
     const outputPath = "/test/" + fileName + ".txt"
     console.log(outputPath)
     
-    const commandToRun = "docker run -d --name " + fileName + " -v /data/greenstamp/analyzer-wcec-api/" + fileName + ":/apks --rm wcec-ubi /bin/bash /apks/"+ fileName + ".apk"+ " 2>&1 | tee /data/greenstamp/wcec/" + fileName + ".txt"
+    const commandToRun = "docker run -v /data/greenstamp/analyzer-wcec-api/" + fileName + ":/apks --rm wcec-ubi /bin/bash /apks/"+ fileName + ".apk"+ " 2>&1 | tee /data/greenstamp/wcec/" + fileName + ".txt"    
     console.log(commandToRun)
 
     console.log("tests: " + tests)
